034512b59829bc39152333295763540ed74cb18d
Hippolyte <93048674+ava-hip@users.noreply.github.com>
2024-05-28 19:16:41 +0200

    add: components, route, header
    Ajout des composants, créations des routes et début du header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "../common/components/home/home.component";
+import {AboutComponent} from "../common/components/about/about.component";
+import {ProjectsComponent} from "../common/components/projects/projects.component";
+import {ContactComponent} from "../common/components/contact/contact.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "home", component: HomeComponent},
+  {path: "about", component: AboutComponent},
+  {path: "projects", component: ProjectsComponent},
+  {path: "contact", component: ContactComponent},
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,12 @@
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
-    "./node_modules/flowbite/**/*.js" 
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require("flowbite/plugin"),
-    require("daisyui"),
   ],
-  daisyui: {
-    themes: true,
-    styled: true,
-    themes: true,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-  }
 }
 
