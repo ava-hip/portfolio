eb15b2380f672a7c98cc0970b046194cd5a664e4
Hippolyte <93048674+ava-hip@users.noreply.github.com>
2024-05-29 01:12:12 +0200

    add: footer + fix du header
    note: css à clean mais c pas mal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import {Component, HostListener} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
-import {HeaderComponent} from "./components/header/header.component";
+import {HeaderComponent} from "../common/header/header.component";
+import {FooterComponent} from "../common/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
